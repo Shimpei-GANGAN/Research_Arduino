1a6de8e59cd3f37572d26ed580a26c137e82eece
gangan <44604921+Shimpei-GANGAN@users.noreply.github.com>
2019-05-13 16:26:59 +0900

    Add sd card dir

---
+++ SDcard/read_SD.ino
@@ -0,0 +1,27 @@
+#include <SD.h>
+
+
+//  CSピン
+const int chipSelect = 4;
+
+void setup() {
+  //シリアルポート初期化
+  Serial.begin(9600);
+  Serial.println(F("Initializind SD card..."));
+
+  pinMode(SS, OUTPUT);
+  SD.begin(chipSelect);
+}
+
+void loop() {
+  File dataFile = SD.open("datalog.txt");
+
+  if ( dataFile ) {
+    while (dataFile.available()){
+      Serial.write(dataFile.read());
+    }
+    dataFile.close();
+  }
+  delay(2000);
+}
+
---
+++ SDcard/write_SD.ino
@@ -0,0 +1,27 @@
+#include <SD.h>
+
+
+//  CSピン
+const int chipSelect = 4;
+
+void setup() {
+  //シリアルポート初期化
+  Serial.begin(9600);
+  Serial.println(F("Initializind SD card..."));
+
+  pinMode(SS, OUTPUT);
+  SD.begin(chipSelect);
+  SD.remove("datalog.txt");
+}
+
+void loop() {
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if ( dataFile ) {
+    int value = analogRead(0);
+    dataFile.println(value);
+    dataFile.close();
+  }
+  delay(1000);
+}
+
